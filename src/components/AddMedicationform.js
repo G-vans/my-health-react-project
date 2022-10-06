48c4bccc66d6b41012d7c74a95ad60d0844edfbf
G-vans <jevans.otieno@student.moringaschool.com>
2022-10-06 17:29:08 +0300

    feat: make post request

--- src/components/AddMedicationform.js
+++ src/components/AddMedicationform.js
@@ -1,23 +1,59 @@
+import React, { useState } from "react";
+
+function AddMedicationform({onAddItem}) {
+    const [medication, setMedication] = useState("")
+    const [form, setForm] = useState("");
+    const [retail, setRetail] = useState();
+    const [price, setPrice] = useState();
+    const [savings, setSavings] = useState();
+
+    function handleSubmit(e) {
+        // e.preventDefault();
+        const addFormData = {
+            name: medication,
+            form: form,
+            retail: retail,
+            price: price,
+            savings: savings,
+        };
+
+        fetch("http://localhost:3001/medications", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(addFormData),
+        })
+            .then((res) => res.json())
+            .then((newForm) => onAddItem(newForm))
+            .catch((err) => console.log(err))
+        alert("New Medication added");
+    }
 
 
-function AddMedicationform() {
+    function handleChange(event) {
+        setMedication({
+            ...medication,
+            [event.target.name]: event.target.value,
+        });
+    }
     return (
         <div className="Header-search">
             <h3>All Medications</h3>
             <p>Add new medication below</p>
 
             <div>
-                <form className="Add-form">
+                <form className="Add-form" onSubmit={handleSubmit}>
                     <div className='inline fields'>
-                        <input type='text' name='medication' placeholder="Medication name" />
-                        <select name="form">
+                        <input onChange={(e) => setMedication(e.target.value)} id="medication" value={medication} type='text' name='medication' placeholder="Medication name" />
+                        <select onChange={(e) => setForm(e.target.value)} id="form" value={medication.form} name="form">
                             <option value=''></option>
                             <option value='tablet'>Tablet</option>
                             <option value='capsule'>Capsule</option>
                         </select>
-                        <input type='number' name='amount' placeholder='Retail price' step='0.01' />
-                        <input type='number' name='amount' placeholder='Our price' step='0.01' />
-                        <input type='number' name='amount' placeholder='Savings' step='0.01' />
+                        <input onChange={(e) => setRetail(e.target.value)} id="retail" value={retail} type='number' name='retail' placeholder='Retail price' step='0.01' />
+                        <input onChange={(e) => setPrice(e.target.value)} value={price} type='number' name='price' placeholder='Our price' step='0.01' />
+                        <input onChange={(e) => setSavings(e.target.value)} value={savings} type='number' name='savings' placeholder='Savings' step='0.01' />
                     </div>
                     <br />
                     <button className='Btn-form' type='submit'>
