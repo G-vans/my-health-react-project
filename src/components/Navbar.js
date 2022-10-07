1cf1b751b1789d6f1666d9a3a05549ccaef62fdc
G-vans <jevans.otieno@student.moringaschool.com>
2022-10-07 07:47:08 +0300

    fix:update navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,15 @@ export default function Navbar() {
             <div className='Logo'>
                 <ul>
                     <li>
-                        <h1><a href='./'>My-Health</a></h1>
+                        <h1><Link to='/'>My-Health</Link></h1>
                     </li>
                 </ul>
             </div>
             <div className='Nav-right'>
                 <ul>
-                    <li><Link to=''>For Providers</Link></li>
-                    <li><Link to='/'>Medications</Link></li>
-                    <li><a href='/doctors'>Contact Doctors</a></li>
+                    <li><Link to='/'>For Providers</Link></li>
+                    <li><Link to='/medications'>Medications</Link></li>
+                    <li><Link to='/doctors'>Doctors</Link></li>
                     <li>
                         <button className="Btn-navbar" ><a href='./'>Sign Up</a></button>
                     </li>
